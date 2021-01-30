7ea60048748fbb2a45d058fbaaf0cf5a23a738a9
Carl Humphreys <humphreys.carl.d@gmail.com>
2021-01-30 18:29:09 +0000

    social icons

--- src/components/common/SocialButton.js
+++ src/components/common/SocialButton.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { FaInstagram, FaFacebookSquare, FaTwitter, FaPinterest } from "react-icons/fa";
+const SocialButton = ({ size = "sm", network = "" }) => {
+    let iconSize = 12;
 
-const SocialButton = () => {
-    return <div></div>;
+    switch (size) {
+        case "sm":
+            iconSize = 16;
+            break;
+        case "md":
+            iconSize = 32;
+            break;
+        case "lg":
+            iconSize = 48;
+            break;
+        default:
+            break;
+    }
+
+    const FACEBOOK = "facebook";
+    const TWITTER = "twitter";
+    const INSTAGRAM = "instagram";
+    const PINTEREST = "pinterest";
+
+    let icon;
+    const props = { color: "white", size: iconSize };
+    switch (network.toLowerCase()) {
+        case FACEBOOK:
+            icon = <FaFacebookSquare {...props} />;
+            break;
+        case TWITTER:
+            icon = <FaTwitter {...props} />;
+            break;
+        case INSTAGRAM:
+            icon = <FaInstagram {...props} />;
+            break;
+        case PINTEREST:
+            icon = <FaPinterest {...props} />;
+            break;
+
+        default:
+            break;
+    }
+
+    return icon;
 };
 
 export default SocialButton;
