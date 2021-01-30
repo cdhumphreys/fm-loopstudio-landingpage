dfdc6c5b6b7e48252b34cdf536d0dd3232055cf3
Carl Humphreys <humphreys.carl.d@gmail.com>
2021-01-30 17:54:32 +0000

    company description section

--- src/components/CompanyDescription.js
+++ src/components/CompanyDescription.js
@@ -1,15 +1,30 @@
 import React from "react";
+import Container from "./common/Container";
+import SectionHeader from "./common/SectionHeader";
 
 const CompanyDescription = () => {
+    const mobileInteractiveImageSrc = `${process.env.PUBLIC_URL}/images/mobile/image-interactive.jpg`;
+    const desktopInteractiveImageSrc = `${process.env.PUBLIC_URL}/images/desktop/image-interactive.jpg`;
     return (
-        <section>
-            <img src="" alt="" srcset="" />
-            <h1>The leader in interactive VR</h1>
-            <p>
-                Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the
-                best companies around the globe. Our award-winning creations have transformed businesses through digital
-                experiences that bind to their brand.
-            </p>
+        <section className="bg-white mt-24 mb-20">
+            <Container className="flex flex-col jsutify-center items-center">
+                <img
+                    className="relative"
+                    src={mobileInteractiveImageSrc}
+                    alt="Interative VR"
+                    srcSet={`${desktopInteractiveImageSrc} 768w`}
+                />
+                <div className="flex flex-col">
+                    <div className="pt-8 pb-4">
+                        <SectionHeader text="The leader in interactive VR" />
+                    </div>
+                    <p className="text-gray pb-8 leading-7 tracking-wider text-center">
+                        Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of
+                        the best companies around the globe. Our award-winning creations have transformed businesses
+                        through digital experiences that bind to their brand.
+                    </p>
+                </div>
+            </Container>
         </section>
     );
 };
