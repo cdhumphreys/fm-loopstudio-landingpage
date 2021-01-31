ce3728c053ea9e24eff8ef0653b8cdc0e9ce63ae
Carl Humphreys <humphreys.carl.d@gmail.com>
2021-01-31 16:35:13 +0000

    mobile menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./common/Logo.js";
 import NavItem from "./common/NavItem.js";
+import { FaBars, FaTimes } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 const Nav = () => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
     const navItems = [
         { href: "#", text: "About" },
         { href: "#", text: "Careers" },
@@ -18,8 +21,34 @@ const Nav = () => {
                     <NavItem key={item.text} href={item.href} text={item.text} />
                 ))}
             </ul>
+            <div onClick={() => setMenuIsOpen(true)} className="md:hidden">
+                <FaBars color="white" size={24} />
+            </div>
+            <div
+                className={`fixed top-0 left-0 right-0 bottom-0 bg-black p-8 z-20 opacity-100 transition-opacity ${
+                    !menuIsOpen && "invisible opacity-0"
+                }`}
+            >
+                <div onClick={() => setMenuIsOpen(false)} className="w-full flex flex-row items-center justify-between">
+                    <Logo />
+                    <FaTimes color="white" size={30} />
+                </div>
+                <ul className="h-full flex flex-col justify-center items-start gap-3">
+                    {navItems.map((item) => (
+                        <NavItem
+                            key={item.text}
+                            href={item.href}
+                            text={item.text}
+                            className="text-3xl font-heading uppercase font-light tracking-widest"
+                        />
+                    ))}
+                </ul>
+            </div>
         </nav>
     );
 };
 
+// Nav.propTypes = {
+//     openMobileMenu: PropTypes.func.isRequired,
+// };
 export default Nav;
